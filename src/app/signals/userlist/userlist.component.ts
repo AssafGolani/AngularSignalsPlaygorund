46b9d6cc9e52a1cfcf488c029c7a6a64c30661f0
Assaf <1214assaf@gmail.com>
2024-02-14 10:44:43 +0200

    temp commit

---
+++ src/app/signals/userlist/userlist.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-userlist",
+  templateUrl: "./userlist.component.html",
+  styleUrls: ["./userlist.component.css"],
+  standalone: true,
+})
+export class UserlistComponent {}
